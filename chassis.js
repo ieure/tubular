fbdc83fdf376cd9247a3d0c72c459ad402e25a34
Ian Eure <ian.eure@gmail.com>
2013-05-26 20:11:23 -0700

    Add chassis info.

---
+++ chassis.js
@@ -0,0 +1,35 @@
+var chassis = {
+    "Wells-Gardner": {
+        "13K7203": {
+            "tube": "A34KQW42X",
+            "yoke": {
+                "h": 3.6,
+                "v": 12.8
+            }
+        },
+        "13K7801": {
+            "tube": "A34Jll40X25",
+            "yoke": {
+                "h": 3.6,
+                "v": 14.6
+            }
+        },
+        "19K7000": {
+            "tube": "A48ABK05X",
+            "yoke": {
+                "h": 2.9,
+                "v": 13.4
+            }
+        }
+    },
+
+    "Electrohome": {
+        "13G07-FBO": {
+            "tube": "370ESB22",
+            "yoke": {
+                "h": undefined,
+                "v": undefined
+            }
+        }
+    }
+};
